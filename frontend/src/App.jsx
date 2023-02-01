47cfc18383ac626870750cb5b4305bba6c4104d6
Cassandra Blondez <98890282+CassandraBZ@users.noreply.github.com>
2023-02-01 17:47:21 +0100

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "@components/Navbar";
+import { Button } from "@material-tailwind/react";
+import Landing from "./pages/Landing";
+
+export default function App() {
+  return (
+    <div className="flex flex-col justify-between w-full">
+      <Navbar />
+      <h1 className="text-3xl font-bold underline text-yellow-500	bg-red-500	">
+        Hello world!
+      </h1>
+      <Button className="bg-indigo-400">Button</Button>
+      <Landing />
+    </div>
+  );
+}
