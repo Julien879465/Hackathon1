8f4f8e7d8c3891af69af4d437fd85c03d977fcc5
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-02-02 13:05:39 +0100

    waiting for db, desgin done

--- frontend/src/pages/MangerFeu.jsx
+++ frontend/src/pages/MangerFeu.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import back from "../assets/icons/chevron-left.svg";
+import Pizzeria from "../assets/img/Pizzeria.png";
 
 function MangerFeu() {
+  // faire le fetch ici en filtrant sur feu
+
   return (
     <>
-      <div className="flex flex-col items-start justify-around w-full p-auto px-7">
+      <div className="flex flex-col items-start justify-around w-full h-full p-auto px-7">
         <div className="flex flex-row w-full justify-evenly items-center">
           <button
             type="button"
@@ -16,6 +19,24 @@ function MangerFeu() {
           <h1 className="py-5 font-sans font-semibold text-xl">
             MANGER ? Manger feu ! 🔥
           </h1>
+        </div>
+
+        <div className="flex flex-col justify-around h-full p-auto m-auto">
+          {/* faire le map ici */}
+          <div className="bg-white rounded-xl p-auto">
+            <img src={Pizzeria} alt="Pizza" />
+            <div className="flex justify-between font-sans font-semibold p-3">
+              <h2 className="text-base">Nom du resto</h2>
+              <p className="text-xl">4/5</p>
+            </div>
+          </div>
+          <div className="bg-white rounded-xl p-auto">
+            <img src={Pizzeria} alt="Pizza" />
+            <div className="flex justify-between font-sans font-semibold p-3">
+              <h2 className="text-base">Nom du resto</h2>
+              <p className="text-xl">4/5</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
