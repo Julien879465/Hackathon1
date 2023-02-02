bfe27d5fccb132c93e25fb8c62d6885323a3eb47
Julien879465 <beillonnet@laposte.net>
2023-02-02 15:00:26 +0100

    route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { UserContextProvider } from "./contexts/UserContext";
 import Personnages from "./pages/Personnages";
+import Manger from "./pages/Manger";
 
 export default function App() {
   return (
     <UserContextProvider>
-      <div className="flex flex-col items-center h-full w-full">
-      <Routes>
-        <Route path="/personnages" element={<Personnages />} />
-      </Routes>
+      <div className="flex flex-col items-center h-full w-full bg-bg-sable">
+        <Routes>
+          <Route path="/manger" element={<Manger />} />
+          <Route path="/personnages" element={<Personnages />} />
+        </Routes>
       </div>
     </UserContextProvider>
   );
