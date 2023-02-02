0ebe5c4d6ae3bd01fb4687158bd9d81032f1fa6d
Julien879465 <beillonnet@laposte.net>
2023-02-02 19:36:17 +0100

    img restaurants2

--- frontend/src/pages/MangerFeuille.jsx
+++ frontend/src/pages/MangerFeuille.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import back from "../assets/icons/chevron-left.svg";
-import Pizzeria from "../assets/img/Pizzeria.png";
 
 function MangerFeuille() {
   // faire le fetch ici en filtrant sur feu
