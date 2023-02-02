92c51b33e6de802e128a549e04fb3b185c9d0499
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-02-02 12:40:41 +0100

    Lateral padding added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { UserContextProvider } from "./contexts/UserContext";
 export default function App() {
   return (
     <UserContextProvider>
-      <div className="flex flex-col items-center w-full h-full m-auto bg-bg-sable">
+      <div className="flex flex-col items-center w-full h-full m-auto bg-bg-sable px-4">
         <Manger />
       </div>
     </UserContextProvider>
