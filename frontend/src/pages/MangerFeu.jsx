522a53493cac7e6a3dd37e349348ea8db17f4b78
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-02-02 12:52:56 +0100

    go back icon added

--- frontend/src/pages/MangerFeu.jsx
+++ frontend/src/pages/MangerFeu.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import back from "../assets/icons/chevron-left.svg";
 
 function MangerFeu() {
   return (
     <>
       <div className="flex flex-col items-start justify-around w-full p-auto px-7">
-        <h1 className="py-5 font-sans font-semibold text-xl">
-          MANGER ? Manger feu ! 🔥
-        </h1>
+        <div className="flex flex-row w-full justify-evenly items-center">
+          <button
+            type="button"
+            className="bg-white rounded-full h-[30px] w-[30px] flex items-center justify-center"
+          >
+            <img src={back} alt="go back" />
+          </button>
+
+          <h1 className="py-5 font-sans font-semibold text-xl">
+            MANGER ? Manger feu ! 🔥
+          </h1>
+        </div>
       </div>
     </>
   );
