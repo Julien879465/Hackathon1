5a60464aea994cfab64c6975784623bea5d03e06
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-02-02 12:34:40 +0100

    useNavigate WIP

--- frontend/src/pages/Manger.jsx
+++ frontend/src/pages/Manger.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import eucalyptus from "../assets/img/eucalyptus 1.svg";
 
 function Manger() {
+//   const navigate = useNavigate();
+
+// //   const handleClick = (e) => {
+// //     if (e.target.value === "feu") {
+// //       navigate("/MangerFeu");
+// //     }
+// //     if (e.target.value === "feuille") {
+// //       navigate("/MangerFeuille");
+// //     }
+// //   };
   return (
     <>
       <div className="flex flex-col items-start justify-around w-full">
@@ -14,10 +25,20 @@ function Manger() {
         <h1 className="py-10 text-xl font-semibold">MANGER ? Manger.</h1>
       </div>
       <div className="flex flex-col justify-between text-xl font-semibold h-2/5 my-10">
-        <button type="button" className="bg-white py-14 rounded-xl mb-10">
+        <button
+          type="button"
+          className="bg-white py-14 rounded-xl mb-10"
+          value="feu"
+        //   onClick={handleClick}
+        >
           Manger feu ?
         </button>
-        <button type="button" className="bg-white p-14 rounded-xl">
+        <button
+          type="button"
+          className="bg-white p-14 rounded-xl"
+          value="feuille"
+        //   onClick={handleClick}
+        >
           Manger feuille ?
         </button>
       </div>
