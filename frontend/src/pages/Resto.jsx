b9714ed0f84ffb25a74947ba36beeb3851f55875
Nassima Ayadi <gsmicrodev@gmail.com>
2023-02-03 10:42:37 +0100

    finish cards menu

--- frontend/src/pages/Resto.jsx
+++ frontend/src/pages/Resto.jsx
@@ -5,13 +5,12 @@ import axios from "axios";
 import back from "../assets/icons/chevron-left.svg";
 import Restaurant from "@components/restaurant";
 import Navbar from "@components/Navbar";
-import cailloux from "../assets/icons/cailloux.png";
 
 function Resto({ selectedRestaurant }) {
   const [data, setData] = useState([]);
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate("/Resto");
+    navigate("/mangerfeu");
   };
   console.log(selectedRestaurant);
 
@@ -24,41 +23,44 @@ function Resto({ selectedRestaurant }) {
       .catch((err) => console.error(err));
   }, []);
   console.log(data);
-  const handleSelection = (e) => {
-    setSelectedMenu(e.target.key);
-    // navigate("/Resto");
-  };
-  return (
-    <div className="flex flex-col justify-between items-center w-screen h-auto  bg-bg-sable">
-      <button
-        type="button"
-        className="bg-white rounded-full h-[30px] w-[30px] fixed top-10 left-5"
-        onClick={handleClick}
-      >
-        <img src={back} alt="go back" />
-      </button>
 
-      {data
-        .filter((x) => x.restaurant_name === selectedRestaurant)
-        .map((elem) => (
-          <>
-            <li className="list-none" key={elem.id}>
-              <img
-                className="aspect-video rounded-t-lg w-full"
-                src={elem.url}
-                alt="menu"
-              />
-              <h1>{elem.restaurant_name}</h1>
-            </li>
-          </>
-        ))}
-      <p>
-        Ici, manger chaud, manger bon, manger gras. Pizzas, pâtes, beaucoup bon.
-      </p>
-      <div className=" w-11/12  bg-bg-sable">
-        <Restaurant selectedRestaurant={selectedRestaurant} />
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-between items-center w-screen h-auto  bg-bg-sable">
+        <button
+          type="button"
+          className="bg-white rounded-full h-[30px] w-[30px] top-10 left-5 fixed"
+          onClick={handleClick}
+        >
+          <img src={back} alt="go back" />
+        </button>
+
+        {data
+          .filter((x) => x.restaurant_name === selectedRestaurant)
+          .map((elem) => (
+            <>
+              <li className="list-none" key={elem.id}>
+                <img
+                  className="aspect-video w-full"
+                  src={elem.url}
+                  alt="menu"
+                />
+                <h1 className="font-extrabold mt-3 mx-7">
+                  {elem.restaurant_name}
+                </h1>
+              </li>
+            </>
+          ))}
+        <p className="m-2 mx-7 text-gray-700">
+          Ici, manger chaud, manger bon, manger gras. Pizzas, pâtes, beaucoup
+          bon.
+        </p>
+        <div className=" w-11/12  bg-bg-sable">
+          <Restaurant selectedRestaurant={selectedRestaurant} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
